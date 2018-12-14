fa5b9a63921f8f88d19199d8a60398f486a144de
Prénom Nom <prenom.nom@server.com>
2018-12-14 17:07:29 +0100

    suite 14/12

--- project.py
+++ project.py
@@ -1,46 +1,24 @@
 import pandas as pd
 from collections import OrderedDict
+import matplotlib.pyplot as plt
+import glob
 
-texte="""Souvent, pour s’amuser, les hommes d’équipage
-Prennent des albatros, vastes oiseaux des mers,
-Qui suivent, indolents compagnons de voyage,
-Le navire glissant sur les gouffres amers.
-
-À peine les ont-ils déposés sur les planches,
-Que ces rois de l'azur, maladroits et honteux,
-Laissent piteusement leurs grandes ailes blanches
-Comme des avirons traîner à côté d'eux.
-
-Ce voyageur ailé, comme il est gauche et veule !
-Lui, naguère si beau, qu'il est comique et laid !
-L'un agace son bec avec un brûle-gueule,
-L'autre mime, en boitant, l'infirme qui volait !
-
-Le Poète est semblable au prince des nuées
-Qui hante la tempête et se rit de l'archer ;
-Exilé sur le sol au milieu des huées,
-Ses ailes de géant l'empêchent de marche"""
 
 #texte = list(albatros) #mis en liste pour travailler sur les chaines de caracteres et les modifier
 # "".join(texte) rétablit en texte
 
-texte_low=texte.lower()
+
 
 def remove_punctuation(chaine):
 #faire docstring
     newchaine=''
     for caractere in chaine:
-        if not (caractere in [',','.',':',';','!','=','+','-', '_', '/', '(', ')', "'",'"',"{","}"]):
+        if not (caractere in [',','.',':',';','!','?','=','+','-', '_', '/', '(', ')', "'",'"',"{","}", '<', '>', '\n',]):
             newchaine += caractere
     return newchaine
 
-texte=remove_punctuation(texte_low)
-
 dico={}
-
-
-
-def prop_lettres(texte):
+def proportion_lettres(texte_lettres):
 #faire docstring donne la proportion de chaque lettre dans le texte, et retourne un dictionnaire
     for lettre in texte:
         if lettre in ['à','â']:
@@ -56,8 +34,35 @@ def prop_lettres(texte):
         dico[lettre] = [(texte.count(lettre))/len(texte)]
     return dico
 
-dico_prop = prop_lettres(texte)
 
-tableau_prop = pd.DataFrame.from_dict(dico_prop)
+poemes_anglais = glob.glob('Dickinson/*.txt')
+poemes_francais = glob.glob('Apollinaire/*.txt')
 
-print(tableau_prop)
+#un peu inutile comme on a mis les textes dans un fichier; à modifier
+#print ('est-ce un texte à tester? (y/n)')
+#rep_test= input()
+#if rep_test == 'y':
+#    print('ok testons le') #faire suite
+#else:
+#    print('les textes pour enregistrer des données sont-il francais ou anglais? (f/a)')
+#    rep_langue= input()
+#    if rep_langue == 'f':
+#        poemes = poemes_francais
+#    if rep_langue == 'a':
+#        poemes = poemes_anglais
+
+
+dico_de_textes={}
+
+for poeme in poemes_francais:
+    with open (poeme) as t:
+        texte = t.read()
+        texte_low = texte.lower()
+        texte_lettres = remove_punctuation(texte_low)#jusqu'ici ok mais apres compte des majuscules
+        dico_prop = proportion_lettres(texte_lettres)# puis met les mproportions du dernier texte pour tous les textes dans le dico_de_textes, 
+        dico_de_textes['{}'.format(poeme)]= dico_prop
+
+
+#print(dico_de_textes)
+tableau_prop = pd.DataFrame.from_dict(dico_de_textes)
+#print(tableau_prop)
