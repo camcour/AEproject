a41d9ae7f0164acb1ae7767251d5628f49a9279e
Prénom Nom <prenom.nom@server.com>
2018-12-21 16:26:50 +0100

    remove_punctuation updated

--- project.py
+++ project.py
@@ -2,7 +2,8 @@ import pandas as pd
 from collections import OrderedDict
 import matplotlib.pyplot as plt
 import glob
-
+import string
+import re
 
 #texte = list(albatros) #mis en liste pour travailler sur les chaines de caracteres et les modifier
 # "".join(texte) rétablit en texte
@@ -13,14 +14,15 @@ def remove_punctuation(chaine):
 #faire docstring
     newchaine=''
     for caractere in chaine:
-        if not (caractere in [',','.',':',';','!','?','=','+','-', '_', '/', '(', ')', "'",'"',"{","}", '<', '>', '\n',]):
+        if not (caractere in [',','.',':',';','!','?','=','+','-', '_', '/', '(', ')', "'",'"',"{","}", '<', '>', '\n',' ']): #probleme parce que \n est deux caracteress:
+        #or in string.punctuation
             newchaine += caractere
     return newchaine
 
-dico={}
+dico_prop={}
 def proportion_lettres(texte_lettres):
 #faire docstring donne la proportion de chaque lettre dans le texte, et retourne un dictionnaire
-    for lettre in texte:
+    for lettre in texte_lettres:
         if lettre in ['à','â']:
             lettre = 'a'
         if lettre in ['è','é','ê','ë']:
@@ -31,8 +33,8 @@ def proportion_lettres(texte_lettres):
             lettre = 'i'
         if lettre in ['û','ü']:
             lettre = 'u'
-        dico[lettre] = [(texte.count(lettre))/len(texte)]
-    return dico
+        dico_prop[lettre] = [(texte_lettres.count(lettre))/len(texte_lettres)]
+    return dico_prop
 
 
 poemes_anglais = glob.glob('Dickinson/*.txt')
@@ -58,11 +60,16 @@ for poeme in poemes_francais:
     with open (poeme) as t:
         texte = t.read()
         texte_low = texte.lower()
-        texte_lettres = remove_punctuation(texte_low)#jusqu'ici ok mais apres compte des majuscules
-        dico_prop = proportion_lettres(texte_lettres)# puis met les mproportions du dernier texte pour tous les textes dans le dico_de_textes, 
+        texte_lettres = re.sub(r'[^a-z\n]','',texte_low)
+        print(texte_lettres)
+        dico_prop = proportion_lettres(texte_lettres)
+        #print (dico_prop)
         dico_de_textes['{}'.format(poeme)]= dico_prop
 
 
 #print(dico_de_textes)
+
+
+
 tableau_prop = pd.DataFrame.from_dict(dico_de_textes)
-#print(tableau_prop)
+print(tableau_prop)
