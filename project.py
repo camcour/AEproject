2025da12aae4b5caebe58eebab64865752e4bfa0
Camille <camillecourgeon@gmail.com>
2019-01-11 15:38:44 +0100

    new dataframe of dico_de_textes

--- project.py
+++ project.py
@@ -9,17 +9,6 @@ import re
 # "".join(texte) rétablit en texte
 
 
-
-def remove_punctuation(chaine):
-#faire docstring
-    newchaine=''
-    for caractere in chaine:
-        if not (caractere in [',','.',':',';','!','?','=','+','-', '_', '/', '(', ')', "'",'"',"{","}", '<', '>', '\n',' ']): #probleme parce que \n est deux caracteress:
-        #or in string.punctuation
-            newchaine += caractere
-    return newchaine
-
-
 def proportion_lettres(texte_lettres):
 #faire docstring donne la proportion de chaque lettre dans le texte, et retourne un dictionnaire
     dico_prop={}
@@ -65,25 +54,35 @@ for recueil in recueils_deux_langues:
         if recueil== poemes_anglais:
             dico_de_textes_an[poeme]= dico_prop
 
-print(dico_prop)
-tableau_prop_fr = pd.DataFrame.from_dict(dico_de_textes_fr)
-tableau_prop_an = pd.DataFrame.from_dict(dico_de_textes_an)
+tableau_prop_fr = pd.DataFrame()
+tableau_prop_an = pd.DataFrame()
 
 
-print(tableau_prop_an.mean(axis=1)) #probleme parce que c'est les valeurs sotn des cases de dico_prop
+for poeme, distrib in dico_de_textes_fr.items():
+    #changer le nom des lignes
+    tableau_prop_fr=pd.concat([tableau_prop_fr,pd.DataFrame(distrib)])
+
+for poeme, distrib in dico_de_textes_an.items():
+    tableau_prop_an=pd.concat([tableau_prop_an,pd.DataFrame(distrib)])
+
+print("Voici un tableau avec les proportions de lettres pour une base de textes français,puis les moyennes d'apparitions de chaque lettre")
+print(tableau_prop_fr)
+print(tableau_prop_fr.mean(axis=0))
+
+print("Voici un tableau avec les proportions de lettres pour une base de textes anglais, puis les moyennes d'apparitions de chaque lettre")
+print(tableau_prop_an)
+print(tableau_prop_an.mean(axis=0)) 
 
 
-textes_test = glob.glob('Testables/*.txt')
+dico_de_texte_inc = {}
 
 for inconnu in textes_test:
     texte_simplifie = simplifie_textes(poeme)
     dico_prop = proportion_lettres(texte_simplifie)
     taille_texte = len(texte_simplifie)
+    dico_de_texte_inc[inconnu]= dico_prop
 
-tableau_prop_inc = pd.DataFrame.from_dict(dico_prop)
-
-print(tableau_prop_fr)
-print(tableau_prop_an)
+tableau_prop_inc = pd.DataFrame.from_dict(dico_de_texte_inc)
 print(tableau_prop_inc)
 
 
@@ -100,3 +99,14 @@ print(tableau_prop_inc)
 #        poemes = poemes_francais
 #    if rep_langue == 'a':
 #        poemes = poemes_anglais
+
+
+
+def remove_punctuation(chaine):
+#faire docstring
+    newchaine=''
+    for caractere in chaine:
+        if not (caractere in [',','.',':',';','!','?','=','+','-', '_', '/', '(', ')', "'",'"',"{","}", '<', '>', '\n',' ']): #probleme parce que \n est deux caracteress:
+        #or in string.punctuation
+            newchaine += caractere
+    return newchaine
