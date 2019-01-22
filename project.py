92a65251ddf29099549e30c60c69c54dc2f8c754
Camille <camillecourgeon@gmail.com>
2019-01-22 14:37:39 +0100

    test done

--- project.py
+++ project.py
@@ -1,10 +1,8 @@
 import pandas as pd
 from collections import OrderedDict
-import matplotlib.pyplot as plt
-import glob
-import string
 import re
-
+import glob
+import random
 
 def proportion_lettres(texte_lettres):
 #faire docstring donne la proportion de chaque lettre dans le texte, et retourne un dictionnaire
@@ -23,7 +21,6 @@ def proportion_lettres(texte_lettres):
         dico_prop[lettre] = [(texte_lettres.count(lettre))/len(texte_lettres)]
     return dico_prop
 
-
 def simplifie_textes(texte_brut):
 #faire une docstring: prend un texte brut, le lie, le rend minuscule et lui enlève toute ponctuation
     with open (texte_brut) as t:
@@ -35,7 +32,12 @@ def simplifie_textes(texte_brut):
 
 poemes_anglais = glob.glob('Dickinson/*.txt')
 poemes_francais = glob.glob('Apollinaire/*.txt')
-textes_test = glob.glob('Testables/*.txt')
+
+textes_test_anglais = glob.glob('test_Anglais/*.txt')
+textes_test_francais = glob.glob('test_Francais/*.txt')
+
+que_tester = random.choice([ textes_test_anglais, textes_test_francais])
+un_texte_inconnu = [random.choice(que_tester)]
 
 recueils_deux_langues= [poemes_francais, poemes_anglais]
 
@@ -44,7 +46,39 @@ dico_de_textes_an = {}
 titres_des_poemes_fr = []
 titres_des_poemes_an = []
 
+def attribue_proportion (ensemble_de_textes):
+#faire docsting
+    dico_prop={}
+    dico_de_texte ={}
+    for texte_brut in ensemble_de_textes:
+        texte_simplifie = simplifie_textes(texte_brut)
+        dico_prop = proportion_lettres(texte_simplifie) #voir si on peut combiner les deux lignes
+        dico_de_texte[texte_brut]=dico_prop
+    return dico_de-texte
+
+def dico_moyennes (dico_de_texte):
+#faire docstring
+    tableau_prop = pd.DataFrame()
+    for texte, distrib in dico_de_texte.items():
+        tableau_prop =pd.concat([tableau_prop, pd.DataFrame(distrib)])
+    dico_moyennes = tableau_prop.mean(axis=0).to_dict()
+    return dico_moyennes
+
+
+
 for recueil in recueils_deux_langues:
+    #if recueil == poemes_francais:
+    #    dico_de_textes_fr_test={}
+    #    dico_de_textes_fr_test = attribue_proportion(poeme, recueil)
+    #if recueil == poemes_anglais:
+    #    dico_de_textes_an_test={}
+    #    dico_de_textes_an_test=  attribue_proportion(poeme, recueil)
+
+    #if recueil == inconnu:
+            #prend un texte au hasard dans la liste des textes inconnu
+    #    dico_de_texte_inc_test = attribue_proportion(poeme, recueil)
+
+    #apres ca c'est ok, juste avant et apres les deux points qui suivent recueils deux langues c'est un test
     for poeme in recueil:
         texte_simplifie = simplifie_textes(poeme)
         dico_prop = proportion_lettres(texte_simplifie)
@@ -58,7 +92,7 @@ for recueil in recueils_deux_langues:
 
 dico_de_texte_inc = {}
 
-for inconnu in textes_test:
+for inconnu in un_texte_inconnu:
     texte_simplifie = simplifie_textes(inconnu)
     taille_texte = len(texte_simplifie)
     dico_de_texte_inc[inconnu]= proportion_lettres(texte_simplifie)
@@ -68,38 +102,53 @@ tableau_prop_an = pd.DataFrame()
 tableau_prop_inc=pd.DataFrame()
 
 for poeme, distrib in dico_de_textes_fr.items(): #changer le nom des lignes
-    tableau_prop_fr =pd.concat([tableau_prop_fr,pd.DataFrame(distrib)]) #ne sert pas de mettre index=titres parce que les refait a chaque fois
+    tableau_prop_fr =pd.concat([tableau_prop_fr,pd.DataFrame(distrib)], sort=True) #ne sert pas de mettre index=titres parce que les refait a chaque fois
 
 for poeme, distrib in dico_de_textes_an.items():
-    tableau_prop_an =pd.concat([tableau_prop_an,pd.DataFrame(distrib)])
+    tableau_prop_an =pd.concat([tableau_prop_an,pd.DataFrame(distrib)], sort=True)
 
 for poeme, distrib in dico_de_texte_inc.items():
-    tableau_prop_inc =pd.concat([tableau_prop_inc,pd.DataFrame(distrib)])
+    tableau_prop_inc =pd.concat([tableau_prop_inc,pd.DataFrame(distrib)], sort=True)
 
-#print(tableau_prop_fr)
-print('tableau moyennes francais')
-tableau_moy_fr = tableau_prop_fr.mean(axis=0)
-print(tableau_moy_fr)
-dict_moy_fr= tableau_moy_fr.to_dict()
+
+dict_moy_fr = tableau_prop_fr.mean(axis=0).to_dict()
+#tableau_moy_fr = tableau_prop_fr.mean(axis=0)
+#dict_moy_fr= tableau_moy_fr.to_dict()
 df_fr=pd.DataFrame(dict_moy_fr,index=['fr'])
 
-print('tableau moy ang')
-tableau_moy_an = tableau_prop_an.mean(axis=0)
-print(tableau_moy_an)
-dict_moy_an = tableau_moy_an.to_dict()
+dict_moy_an = tableau_prop_an.mean(axis=0).to_dict()
 df_an = pd.DataFrame(dict_moy_an,index=['an'])
 
-tableau_moy_inc=tableau_prop_inc.mean(axis=0)
-dict_moy_inc= tableau_moy_inc.to_dict()
-df_inc=pd.DataFrame(dict_moy_inc,index=['inc'])
-print(tableau_moy_inc)
+dict_moy_inc=tableau_prop_inc.mean(axis=0).to_dict()
+df_inc = pd.DataFrame(dict_moy_inc,index=['inc'])
 
-print('corr tout')
-inc_vs_ref=[df_inc, df_fr, df_an]
-tableau_moyennes_fr_an_inc=pd.concat(inc_vs_ref).transpose()
-print(tableau_moyennes_fr_an_inc) #tranpose pour bien aller avec le sens de corr
-print(tableau_moyennes_fr_an_inc['fr'].corr(tableau_moyennes_fr_an_inc['inc'])) #donne le coefficient de correlation entre les colonnes
-print(tableau_moyennes_fr_an_inc['an'].corr(tableau_moyennes_fr_an_inc['inc']))
+print('Le texte testé était', inconnu, '\n')
+
+print("tableau de correlation entre les fréquences d'apparition des lettres par langue, et celles du texte à tester ")
+tableau_moyennes_fr_an_inc=pd.concat([df_inc, df_fr, df_an], sort=True).transpose()
+print(tableau_moyennes_fr_an_inc, '\n') #tranpose pour bien aller avec le sens de corr
+
+correlation_fr_inc = tableau_moyennes_fr_an_inc['fr'].corr(tableau_moyennes_fr_an_inc['inc']) #donne le coefficient de correlation entre les colonnes
+correlation_an_inc = tableau_moyennes_fr_an_inc['an'].corr(tableau_moyennes_fr_an_inc['inc'])
+print('correlation francais/inconnu:',correlation_fr_inc) #donne le coefficient de correlation entre les colonnes
+print('correlation anglais/inconnu:', correlation_an_inc, '\n')
+
+
+#PHASE DE TEST
+
+
+if (correlation_fr_inc > correlation_an_inc) and que_tester == textes_test_francais:
+    print("Il semble être francais, et c'est vrai!")
+elif (correlation_fr_inc > correlation_an_inc) and que_tester == textes_test_francais:
+    print ("Il semble être francais, mais malheureusement il ne l'est pas")
+
+if (correlation_an_inc > correlation_fr_inc) and que_tester == textes_test_anglais:
+    print("Il semble être anglais, et c'est vrai!")
+elif (correlation_fr_inc > correlation_an_inc) and que_tester == textes_test_anglais:
+    print ("Il semble être anglais, mais malheureusement il ne l'est pas")
+
+
+
 
 #VERIFIER QUE CE SONT LES BONNES VALEURS PARCE QUE CORRELATION DANS LE MAUVAIS SENS
 
@@ -127,31 +176,32 @@ tableau_moy= pd.DataFrame()
 
 liste_de_dico_de_textes=(dico_de_textes_fr, dico_de_textes_an, dico_de_texte_inc)
 
-for dico in liste_de_dico_de_textes:
+#for dico in liste_de_dico_de_textes:
 
-    if dico == dico_de_textes_fr:
-        tableau_prop = tableau_prop_fr
-        tableau_moy = tableau_moy_fr
-    if dico == dico_de_textes_an:
-        tableau_prop = tableau_prop_an
-        tableau_moy = tableau_moy_an
-    if dico == dico_de_texte_inc:
-        tableau_prop = tableau_prop_inc
-        tableau_moy = tableau_prop_inc
+#    if dico == dico_de_textes_fr:
+#        tableau_prop = tableau_prop_fr
+#        tableau_moy = tableau_moy_fr
+#    if dico == dico_de_textes_an:
+#        tableau_prop = tableau_prop_an
+#        tableau_moy = tableau_moy_an
+#    if dico == dico_de_texte_inc:
+#        tableau_prop = tableau_prop_inc
+#        tableau_moy = tableau_prop_inc
 
-    for poeme, distrib in dico_de_textes_fr.items(): #changer le nom des lignes
-        tableau_prop =pd.concat([tableau_prop,pd.DataFrame(distrib)])
-
-#POUR COMPARER AVEC MATRICE DISTANCE EUCLIDIENNE This is an old question, but there is a Scipy function that does this:
-
-tableau_moyennes_compar=pd.DataFrame()
+#    for poeme, distrib in dico_de_textes_fr.items(): #changer le nom des lignes
+#        tableau_prop =pd.concat([tableau_prop,pd.DataFrame(distrib)], sort=True)
 
 
-#tableau_moyennes_compar= pd.concat([tableau_moyennes_ref,tableau_prop_inc])
-#print(tableau_moyennes_compar)
 
-from scipy.spatial.distance import pdist, squareform
+#PHASE DE TEST
 
+
+if que_tester == textes_test_anglais :
+    anglais = True
+    francais = False
+if que_tester == textes_test_francais :
+    anglais = False
+    francais = True
 
 
 ## FIN
